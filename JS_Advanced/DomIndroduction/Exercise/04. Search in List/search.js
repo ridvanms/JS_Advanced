0d4e68a2b90608da1ef192348b0d39de359ac552
Ridvan <ridvans@abv.bg>
2023-01-26 21:57:58 +0200

    search

--- JS_Advanced/DomIndroduction/Exercise/04. Search in List/search.js
+++ JS_Advanced/DomIndroduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,24 @@
 function search() {
-   // TODO
+   
+   const townsUl = document.getElementById('towns')
+   const result= document.getElementById('result')
+   const searchInputValue = document.getElementById('searchText')
+
+   let arrOfLists = Array.from(townsUl.children)
+   let counter = 0;
+   for (const li of arrOfLists) {
+      if(li.textContent.toLowerCase().includes(searchInputValue.value.toLowerCase())){
+         li.style.fontWeight = 'bold'
+         li.style.textDecoration = 'underline'
+         counter++
+      }else{
+         li.style.textDecoration = ''
+         li.style.fontWeight = ''
+         
+      }
+   }
+   
+   result.textContent = `${counter} matches found.`
+   counter= 0;
+   searchInputValue.value = ''
 }
