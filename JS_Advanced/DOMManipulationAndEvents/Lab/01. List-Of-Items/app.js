e89b5d654cdc595192ccd7e2bdb7cf06dde9984d
ridvanms <ridvans@abv.bg>
2023-01-28 16:10:03 +0200

    Delete-From-Table

--- JS_Advanced/DOMManipulationAndEvents/Lab/01. List-Of-Items/app.js
+++ JS_Advanced/DOMManipulationAndEvents/Lab/01. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemText = document.getElementById('newItemText')
+    let items = document.getElementById('items')
+
+    let li = document.createElement('li')
+    li.textContent = newItemText.value;
+    
+    items.appendChild(li)
+
+    newItemText.value = ''
 }
--- JS_Advanced/DOMManipulationAndEvents/Lab/02. Delete-From-Table/app.js
+++ JS_Advanced/DOMManipulationAndEvents/Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,25 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let customersContent = document.querySelectorAll('table tbody tr')
+    let resultContent =document.getElementById('result')
+    let emailInput = document.querySelector('input[name="email"]')
+    
+    
+    let trElements = Array.from(customersContent)
+
+    for (const tr of trElements) {
+       
+        let email = tr.children[1]
+        
+        if(email.textContent === emailInput.value){
+            if(emailInput.value === '')  return 
+            tr.remove()
+            resultContent.textContent = 'Deleted'
+        }else {
+            
+            resultContent.textContent = 'Not found.'
+        }
+    }
+    emailInput.value = ''
+
+
 }
