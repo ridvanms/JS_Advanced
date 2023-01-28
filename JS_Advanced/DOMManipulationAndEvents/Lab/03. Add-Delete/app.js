e31ba3772bce1454b13850a3380e91b69cf9e039
ridvanms <ridvans@abv.bg>
2023-01-28 22:30:50 +0200

    Add-Delete

--- JS_Advanced/DOMManipulationAndEvents/Lab/03. Add-Delete/app.js
+++ JS_Advanced/DOMManipulationAndEvents/Lab/03. Add-Delete/app.js
@@ -1,3 +1,18 @@
 function addItem() {
-    //TODO...
+    const newIteminput = document.getElementById('newItemText')
+    const ulContent = document.getElementById('items')
+
+    const  li = document.createElement('li')
+    li.textContent = `${newIteminput.value}`
+    const  a = document.createElement('a')
+    a.setAttribute('href','#')
+    a.textContent = '[Delete]'
+    
+    li.appendChild(a)
+
+    a.addEventListener('click',()=>a.parentElement.remove())
+    
+    ulContent.appendChild(li)
+    newIteminput.value = ''
+
 }
