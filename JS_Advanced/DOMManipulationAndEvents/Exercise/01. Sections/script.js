6cc0efcfa14d02744835ec48dc2cbdafa20f7b9c
ridvanms <ridvans@abv.bg>
2023-01-30 12:06:05 +0200

    Sections

--- JS_Advanced/DOMManipulationAndEvents/Exercise/01. Sections/script.js
+++ JS_Advanced/DOMManipulationAndEvents/Exercise/01. Sections/script.js
@@ -1,3 +1,13 @@
 function create(words) {
-   console.log('TODO:...');
+   let [...arr] = words
+   const contentDocument = document.getElementById('content')
+   for (const text of arr) {
+      const div = document.createElement('div')
+      const p = document.createElement('p')
+      p.textContent = text;
+      p.style.display = 'none'     
+      div.addEventListener('click',()=>p.style.display = 'block')
+      div.appendChild(p);
+      contentDocument.appendChild(div)
+   }  
 }
--- JS_Advanced/DOMManipulationAndEvents/Lab/04. Mouse-Gradient/app.js
+++ JS_Advanced/DOMManipulationAndEvents/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,10 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const result = document.getElementById('result')
+    const gradient = document.getElementById('gradient')
+
+    const gradientMove = (e) => {
+        let percent = Math.floor((e.offsetX/ e.target.offsetWidth) * 100)
+        result.textContent = `${percent}%`
+    }
+    gradient.addEventListener('mousemove',gradientMove)
 }
