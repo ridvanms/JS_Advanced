4793f964fe28233da2d04ab0665de060b23a9fbf
Ridvan <ridvans@abv.bg>
2023-02-04 19:11:48 +0200

    updating

--- JS_Advanced/DomIndroduction/Exercise/05. Table - Search Engine/solution.js
+++ JS_Advanced/DomIndroduction/Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,22 @@
 function solve() {
+   const tdContentsInTBody = document.querySelectorAll('tbody tr td')
+   const searchField = document.getElementById('searchField')
+   
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
+   
+   let tdArr = Array.from(tdContentsInTBody);
    function onClick() {
-      //   TODO:
 
+      for(let i = 0;i<tdArr.length;i+=3){
+         tdArr[i].parentElement.classList.remove('select')
+         
+         if(tdArr[i].textContent.includes(searchField.value)){
+            tdArr[i].parentElement.classList.add('select')
+         }else if(tdArr[i+1].textContent.toLowerCase().includes(searchField.value.toLowerCase())){
+            tdArr[i].parentElement.classList.add('select')
+         }else if(tdArr[i+2].textContent.toLowerCase().includes(searchField.value.toLowerCase())){
+            tdArr[i].parentElement.classList.add('select')
+         }
+      }
    }
 }
