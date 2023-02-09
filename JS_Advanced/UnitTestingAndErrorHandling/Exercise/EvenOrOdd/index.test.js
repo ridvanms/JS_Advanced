33b240e1509931f9e5bc91273890907096224bbc
ridvanms <ridvans@abv.bg>
2023-02-09 11:21:08 +0200

    updating

--- JS_Advanced/UnitTestingAndErrorHandling/Exercise/EvenOrOdd/index.test.js
+++ JS_Advanced/UnitTestingAndErrorHandling/Exercise/EvenOrOdd/index.test.js
@@ -1,17 +1,8 @@
 const isOddOrEven = require('./index.js')
-const {expect}= require('chai')
+const {assert}= require('chai')
 
-describe('isOddOrEven function testing',()=>{
-    it('cecking for odd ',()=>{
-        expect(isOddOrEven('qko') = 'odd')
-    });
-    it('checking for even',()=>{
-        expect(isOddOrEven('nishto') = 'even')});
-    it('checking if it is string',()=>{
-        expect(isOddOrEven(4).to.undefined)});
-    it('checking if it is string 2',()=>{
-        expect(isOddOrEven([]) = undefined)});
-    it('checking if it is string 3',()=>{
-        expect(isOddOrEven({}) = undefined)});
-    
+describe('isOddOrEven',()=>{
+    it('check of even ',()=>{
+        assert.equal(isOddOrEven('qko'),'even')
+    })
 })
