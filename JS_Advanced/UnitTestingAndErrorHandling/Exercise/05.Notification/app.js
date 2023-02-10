1da716d02b01a224c80acab2f850722e4fa6a438
ridvanms <ridvans@abv.bg>
2023-02-10 14:07:28 +0200

    Notification

--- JS_Advanced/UnitTestingAndErrorHandling/Exercise/05.Notification/app.js
+++ JS_Advanced/UnitTestingAndErrorHandling/Exercise/05.Notification/app.js
@@ -1,4 +1,16 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+  const notificationContainer = document.getElementById('notification')
+  
+  if(notificationContainer.style.display !== 'block'){
+    notificationContainer.textContent = 'This is a message'
+    
+    notificationContainer.style.display = 'block'
+  }
+
+  notificationContainer.addEventListener('click',(event)=>{
+    // console.log(event.target)
+    notificationContainer.textContent = ''
+    notificationContainer.style.display = 'none'
+  })  
+  
 }
