796c4d42f4bb35af83884061670f1fd5c5e5a976
ridvanms <ridvans@abv.bg>
2023-02-10 10:28:23 +0200

    looupChair

--- JS_Advanced/UnitTestingAndErrorHandling/Exercise/charLookup/lookupChar.test.js
+++ JS_Advanced/UnitTestingAndErrorHandling/Exercise/charLookup/lookupChar.test.js
@@ -1,2 +1,19 @@
 const lookupChar = require('./charLookup')
 const {expect} = require('chai')
+
+describe('testing lookupChair function',function(){
+    it('Checking passed values',()=>{
+        expect(lookupChar(4,4)).to.equal(undefined)
+        expect(lookupChar('4','4')).to.equal(undefined)
+        expect(lookupChar([],3)).to.equal(undefined)
+        expect(lookupChar(4,[4])).to.equal(undefined)
+    })
+    it('checking for index correction',()=>{
+        expect(lookupChar('love',5)).to.equal('Incorrect index')
+        expect(lookupChar('love',-1)).to.equal('Incorrect index')
+        expect(lookupChar('love',4)).to.equal('Incorrect index')
+    })
+    it('returnning the letter in the proper index',()=>{
+        expect(lookupChar('love',2)).to.equal('v')
+    })
+})
