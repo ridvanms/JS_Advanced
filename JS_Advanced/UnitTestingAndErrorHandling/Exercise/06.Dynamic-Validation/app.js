5d5bb29727f79bf06281ab8f5392a2e7a228826d
ridvanms <ridvans@abv.bg>
2023-02-10 14:40:37 +0200

    Dynamic validation

--- JS_Advanced/UnitTestingAndErrorHandling/Exercise/06.Dynamic-Validation/app.js
+++ JS_Advanced/UnitTestingAndErrorHandling/Exercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    // TODO
+    const email = document.getElementById('email');
+    
+    const PATTERN = /[a-z]+@[a-z]+[.][a-z]+/gm;
+
+    email.addEventListener('change',()=>{
+        if(PATTERN.test(email.value)){
+            email.classList.remove('error')
+        }else{
+            email.classList.add('error')
+        }
+    })
+
+    console.log(email)
 }
