f51a22db40327d20d5cf099eb6c79cb56f45d5fb
ridvanms <ridvans@abv.bg>
2023-02-11 12:00:06 +0200

    pointDistance

---
+++ JS_Advanced/Classes and Attributes/Lab/pointDistance.js
@@ -0,0 +1,13 @@
+function pointDistance(){
+    class Point{
+        constructor(x,y){
+            this.x = x ,
+            this.y = y
+        }
+        static distance(p1,p2){
+            let dx = p1.x - p2.x;
+            let dy = p1.y - p2.y;
+            return Maths.sqrt(dx ** 2 + dy ** 2)
+        }
+    }
+}
