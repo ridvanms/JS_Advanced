05222c5dd47239462e7dcf34b57e94fd9330e467
ridvanms <ridvans@abv.bg>
2023-03-04 23:14:13 +0200

    Forecaster

--- JS_Applications/Asynchronous Programming - Exercise/01.Bus-Stop/app.js
+++ JS_Applications/Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -1,3 +1,30 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+    const stopId = document.getElementById("stopId");
+    let stopNameDiv = document.getElementById('stopName');
+    const busesUl = document.getElementById('buses')
+
+    busesUl.replaceChildren('')
+
+    let busId = stopId.value;
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${busId}`
+
+    const res = await fetch(url);
+    
+    
+    if(res.status !== 200){
+        stopNameDiv.textContent = 'Error'
+        return
+    }
+
+    const data = await res.json();
+
+    stopNameDiv.textContent = data.name;
+
+    for(const [key,value] of Object.entries(data.buses)){
+        const li = document.createElement('li');
+        li.textContent = `Bus ${key} arrives in ${value} minutes`;
+        busesUl.appendChild(li)
+    }
+
+
 }
--- JS_Applications/Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
+++ JS_Applications/Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -1,12 +1,42 @@
 function solve() {
+    const infoDiv = document.getElementById('info');
 
+    const departInput = document.getElementById('depart');
+    const arriveInput = document.getElementById('arrive');
+
+    function toggleBtn(){
+        if(departInput.disabled === false){
+            departInput.disabled = true
+            arriveInput.disabled = false
+        }else{
+            departInput.disabled = false;
+            arriveInput.disabled = true;
+        }
+    }
+    let stopId = {
+        next:'depot'
+    }
+
+    
+    
     function depart() {
-        console.log('Depart TODO...');
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stopId.next}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            stopId = data
+            infoDiv.textContent = `Next stop ${stopId.name}`
+        }).catch(err => infoDiv.textContent = 'Opppps! Something went wrong')
+        toggleBtn();
     }
-
+    
     function arrive() {
-        console.log('Arrive TODO...');
+        infoDiv.textContent = `Arriving at ${stopId.name}`
+        
+        toggleBtn();
     }
+    
 
     return {
         depart,
